a73e30fd018b4fece798c44c485bd9d83279ce54
sharmakhushi282003-eng <sharmakhushi282003@gmail.com>
2026-04-08 19:53:43 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-from environment import EmailEnv
 
 app = FastAPI()
-env = EmailEnv()
 
 @app.get("/")
 def home():
@@ -10,15 +8,8 @@ def home():
 
 @app.post("/reset")
 def reset():
-    return env.reset()
+    return {"message": "New task"}
 
 @app.post("/step")
 def step(action: dict):
-    return env.step(action)
-
-def main():
-    import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
-
-if __name__ == "__main__":
-    main()
+    return {"reward": 0.5, "done": True}
