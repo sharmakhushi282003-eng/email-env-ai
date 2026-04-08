bce721dec93165217857028b3d3444c4414c8980
sharmakhushi282003-eng <sharmakhushi282003@gmail.com>
2026-04-08 17:11:45 +0530

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,28 +1,31 @@
-def step(self, action_dict):
-    action = action_dict.get("action", "ignore")
+class EmailEnv:
 
-    # simple internal tasks mapping
-    if self.current_task == 1:
-        reward = 0.8 if action == "reply" else 0.3
-    elif self.current_task == 2:
-        reward = 0.7 if action == "ignore" else 0.2
-    else:
-        reward = 0.9 if action == "reply" else 0.4
-
-    done = True
-
-    return {
-        "reward": reward,
-        "done": done
-    }
-def reset(self):
-    if not hasattr(self, "current_task"):
-        self.current_task = 1
-    else:
-        self.current_task += 1
-        if self.current_task > 3:
+    def reset(self):
+        if not hasattr(self, "current_task"):
             self.current_task = 1
+        else:
+            self.current_task += 1
+            if self.current_task > 3:
+                self.current_task = 1
 
-    return {
-        "message": f"Email task {self.current_task}"
-    }
+        return {
+            "message": f"Email task {self.current_task}"
+        }
+
+    def step(self, action_dict):
+        action = action_dict.get("action", "ignore")
+
+        # simple internal tasks mapping
+        if self.current_task == 1:
+            reward = 0.8 if action == "reply" else 0.3
+        elif self.current_task == 2:
+            reward = 0.7 if action == "ignore" else 0.2
+        else:
+            reward = 0.9 if action == "reply" else 0.4
+
+        done = True
+
+        return {
+            "reward": reward,
+            "done": done
+        }
